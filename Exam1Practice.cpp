b3afce1f5c8f15961152e7575c89fa04104562ba
Maximillian Yang <myang1@hawkmail.hfcc.edu>
2026-04-08 12:04:06 -0400

    added charge class and override some operators for charge class

--- Exam1Practice.cpp
+++ Exam1Practice.cpp
@@ -2,7 +2,9 @@
 
 // Exam Practice 1
 
-class student {
+class Student {
+
+	
 	private:
 		int studentID;
 		int creditHours;
@@ -11,7 +13,7 @@ class student {
 		bool online;
 	
 	public:
-		student(int studentID, int creditHours, std::string track, std::string timing, bool online)
+		Student(int studentID, int creditHours, std::string track, std::string timing, bool online)
 			:studentID(studentID), creditHours(creditHours), track(track), timing(timing), online(online)
 			{}
 				
@@ -38,11 +40,80 @@ class student {
 			this->online = online;
 		}	
 			
-				
+		
 				
 };
 
+
+
+
+class Charge{
+	
+	friend std::ostream& operator<<(std::ostream&, Charge oneCharge);
+	
+	private:
+		std::string label;
+		double charge;
+		
+		
+	public:
+		Charge(std::string label, double charge)
+		:label(label), charge(charge)
+		{}
+		
+
+		std::string getLabel()	{	return label;	}
+		double getCharge()	{	return charge;	}
+		
+		
+		void setLabel(std::string label){
+			this->label = label;
+		}
+		void setLabel(double label){
+			this->label = label;
+		}
+		
+	
+		float operator-(float discountPercentage){
+			return charge * discountPercentage;
+		}
+		
+		float operator+(float discountPercentage){
+			return charge * discountPercentage;
+		}
+	
+		//returning a charge class inside the class
+		Charge operator*(int credits){
+			
+			Charge oneCharge("Base Tuition", this->charge*credits);
+			
+			return oneCharge;
+			
+		}
+	
+};
+
+std::ostream& operator<<(std::ostream& o, Charge oneCharge)
+{
+	o<<oneCharge.label<<" \t $ "<<oneCharge.charge;	
+}
+
+
+
+
+
+
 int main(){
+	
+	Charge newCharge("payment", 10);
+	
+	float discountPercent = .10;
+	
+	std::cout<<"\n"<<newCharge-discountPercent;//retunrs 100 beacuse 10*10 is 100
+	std::cout<<"\n"<<newCharge+discountPercent;
+	
+	Charge newCharge2 = newCharge*(10);
+	std::cout<<"\n"<<newCharge2.getCharge();
 	
 	return 0;
 }
