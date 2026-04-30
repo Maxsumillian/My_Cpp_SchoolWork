4378feedb620fbc64325024783275e5fad8e745f
Maximillian Yang <myang1@hawkmail.hfcc.edu>
2026-04-30 11:51:44 -0400

    learned how to make templates!
    Signed-off-by: Maximillian Yang <myang1@hawkmail.hfcc.edu>

--- Templates.cpp
+++ Templates.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
 
 
-int add(int x, int y);
+template <class T>
+class Item {
+	private:
+		T value;
+		
+	public:
+		Item(T n): value (n){}
+		
+		T getValue(){return value;}
+		
+		
+		void displayValue(){
+			std::cout<<value;
+		}
+};
 
-int add(float x, float y){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+template <typename T>
+T add(T x, T y)
+{
 	return x + y;
-};
-std::string add(std::string x, std::string y){
-	return x + y;
-};
+}
+
+template <typename T, typename U>
+bool compare(T x, U y)
+{
+	return x > y;
+}
+
+
+
+//int add(int x, int y){
+//	return x + y;
+//};
+//int add(float x, float y){
+//	return x + y;
+//};
+//std::string add(std::string x, std::string y){
+//	return x + y;
+//};
 
 int main(){
 	std::string first="kim", last="Mosc";
@@ -16,11 +61,8 @@ int main(){
 	float x = 2.8, y = 3.7;
 	std::cout<<"\nInts: "<<add(a,b); 
 	std::cout<<"\nFloats: "<<add(x,y); 
-	std::cout<<"\nInts: "<<add(a,b); 
+	std::cout<<"\nString: "<<add(first,last); 
 	
 };
 
-int add(int x, int y){
-	return x + y;
-};
 
